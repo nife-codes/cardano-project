d98c8609e8c2ab0b1ff5b36cc0d09c4f7031be69
nife-codes <hephzibbahmorayo@gmail.com>
2026-02-06 08:27:11 +0100

    Backend transfer

--- backend/tracker/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Manufacturer, Distributor, Pharmacy, Batch, Transaction
-
-admin.site.register(Manufacturer)
-admin.site.register(Distributor)
-admin.site.register(Pharmacy)
-admin.site.register(Batch)
-admin.site.register(Transaction)
